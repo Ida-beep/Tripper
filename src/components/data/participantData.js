9d2ca210111e57d6ac4b50feebd28a8ba4affe04
idavill <idamariavilladsen@gmail.com>
2021-11-06 11:31:32 +0100

    add participantData.js as placeholder data

---
+++ src/components/data/participantData.js
@@ -0,0 +1,44 @@
+const participantData =[
+      {
+        "id":1,
+        "name": "Jakob",
+        "age": 32,
+        "dutypreferences": [
+          "Cleaning",
+          "Cooking",
+          "Shopping"
+        ]
+      },
+      {
+        "id":2,
+        "name": "Ingrid",
+        "age": 23,
+        "dutypreferences": [
+          "Entertainment",
+          "Shopping",
+          "Kitchen Support"
+        ]
+      },
+      {
+        "id":3,
+        "name": "Ida",
+        "age": 27,
+        "dutypreferences": [
+          "Cooking",
+          "Kitchen Support",
+          "Shopping"
+        ]
+      },
+      {
+        "id":4,
+        "name": "Malene",
+        "age": 34,
+        "dutypreferences": [
+          "Shopping",
+          "Kitchen Support",
+          "Cleaning"
+        ]
+      }
+]
+
+export default participantData;
