e9d84d7271c338ec8a8552b55d6f8bbf672a5ce1
idavill <idamariavilladsen@gmail.com>
2021-11-07 12:06:47 +0100

    create pop up for new family members

---
+++ src/components/data/dutyList.js
@@ -0,0 +1,20 @@
+const duties =[
+    {
+      "name":"Cooking",
+      "min-people-required": 10,
+    },
+    {
+        "name":"Cleaning",
+        "min-people-required": 5,
+    },
+    {
+        "name":"Games",
+        "min-people-required": 3,
+    },
+    {
+        "name":"Kitchen",
+        "min-people-required": 7,
+    },
+];
+
+export default duties;
