a7ca25ae4edc3b82cc22d680e0c5139b2804a4ba
idavill <idamariavilladsen@gmail.com>
2021-11-09 17:06:51 +0100

    Added Data for Jakob

--- src/components/data/participantData.js
+++ src/components/data/participantData.js
@@ -6,6 +6,14 @@ const participantData =[
         "name": "Jakob Thomsen",
         "age": 32,
         "carseat": "---",
+        "address":{
+          "street":"Nørrebrogade 51",
+          "zip": "2200, København N"
+        },
+        "email":"jakob@gmail.com",
+        "mobile":30858590,
+        "phone":70706647,
+        "workphone":50927372,
         "dutypreferences": [
           "Cleaning",
           "Cooking",
