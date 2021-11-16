69cf46525dcd8808f48fba951bc6ee3938a27965
idavill <idamariavilladsen@gmail.com>
2021-11-16 11:08:24 +0100

    add try-catch

--- src/components/API.js
+++ src/components/API.js
@@ -6,21 +6,25 @@ function initialize(){
 }
 
 function addFamilyMember({firstName, lastName, age, duties}){
-    const FamilyMember = Parse.Object.extend("familyMember");
-    
-    const familyMember = new FamilyMember();
-    familyMember.set("firstName",firstName);
-    familyMember.set("lastName",lastName);
-    familyMember.set("age",age);
-    familyMember.set("duties",duties);
+    try{
+        const FamilyMember = Parse.Object.extend("familyMember");
+        const familyMember = new FamilyMember();
+        familyMember.set("firstName",firstName);
+        familyMember.set("lastName",lastName);
+        familyMember.set("age",age);
+        familyMember.set("duties",duties);
 
-    familyMember.save()
-    .then((familyMember)=>{
-        alert("A Family Member was submitted: " + familyMember.firstName); 
-    }, (error)=> {
-        alert("Failed to create object, error code: " + error.message);
-    });
-    console.log("saved family member")
+        familyMember.save()
+        .then((familyMember)=>{
+            alert("A Family Member was submitted: " + familyMember.firstName); 
+        }, (error)=> {
+            alert("Failed to create object, error code: " + error.message);
+        });
+        console.log("saved family member")
+
+    } catch(error){
+        console.log(error);
+    }
 }
 
 export default {initialize:initialize,addFamilyMember:addFamilyMember};
