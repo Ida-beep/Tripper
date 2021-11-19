c4c0a24e993893d989803f35dedf60745a7740e6
idavill <idamariavilladsen@gmail.com>
2021-11-19 16:46:13 +0100

    fix API, add func for add ContactPerson

--- src/components/API.js
+++ src/components/API.js
@@ -1,5 +1,8 @@
 import { Parse } from 'parse';
 
+/**
+ *  @public initializes connection with backedn Back4App 
+ */
 function initialize(){
     Parse.initialize(process.env.REACT_APP_APP_KEY, process.env.REACT_APP_JS_KEY);
     Parse.serverURL = process.env.SERVER_URL;
@@ -7,7 +10,7 @@ function initialize(){
 
 function addFamilyMember({firstName, lastName, age, duties}){
     try{
-        const FamilyMember = Parse.Object.extend("familyMember");
+        const FamilyMember = Parse.Object.extend("FamilyMember");
         const familyMember = new FamilyMember();
         familyMember.set("firstName",firstName);
         familyMember.set("lastName",lastName);
@@ -20,7 +23,32 @@ function addFamilyMember({firstName, lastName, age, duties}){
         }, (error)=> {
             alert("Failed to create object, error code: " + error.message);
         });
-        console.log("saved family member")
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+function addContçactPerson({firstName,lastName,age,duties,email,address,workphone,phone,mobile}){
+    try{
+        const ContactPerson = Parse.Object.extend("ContactPerson");
+        const contactPerson = new ContactPerson();
+        contactPerson.set("firstName",firstName);
+        contactPerson.set("lastName",lastName);
+        contactPerson.set("age",age);
+        contactPerson.set("duties",duties);
+        contactPerson.set("email",email);
+        contactPerson.set("address",address);
+        contactPerson.set("workphone",workphone);
+        contactPerson.set("phone",phone);
+        contactPerson.set("mobile",mobile);
+
+        contactPerson.save()
+        .then((contactPerson)=>{
+            alert("A Family Member was submitted: " + contactPerson.firstName); 
+        }, (error)=> {
+            alert("Failed to create object, error code: " + error.message);
+        });
 
     } catch(error){
         console.log(error);
