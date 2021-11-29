ab7b4b87cb4a5c6dabfc20fe6f151ea5103e4b05
idavill <idamariavilladsen@gmail.com>
2021-11-29 11:23:35 +0100

    Dropdown Menu on Excursion (needs refactoring)

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 /**
  *  @public NavMenu let's the user use the NavRouter to switch between different main pages
  */
 
-function NavMenu () {
-    return <div>
+function NavMenu() {
+  
+  const [dropDown, setDropdown] = useState(false);
+  
+  return (
+    <>
         <nav className="Nav-Bar">
             <ul className="Nav-Ul">
               <div className="Logo">tripper</div>
@@ -16,7 +20,15 @@ function NavMenu () {
                     <NavLink to="/"> Home </NavLink>
                 </li>
                 <li className="Nav-Item">
-                  <NavLink to="/Excursion">Excursion</NavLink>
+                  <NavLink to="/Excursion" onMouseEnter={() => setDropdown(!dropDown)}>Excursion</NavLink>
+                    {dropDown && <div className="nav-drop-down" onMouseLeave={() => setDropdown(!dropDown)}>
+                      <li className="drop-down-options">
+                        <NavLink to="Duties">Shoppinglist</NavLink>
+                      </li>
+                      <li className="drop-down-options">
+                        <NavLink to="Duties">Duties</NavLink>
+                      </li>
+                    </div>}
                 </li>
                 <li className="Nav-Item">
                   <NavLink to="/Profile">Profile</NavLink>
@@ -24,6 +36,7 @@ function NavMenu () {
               </div>
             </ul>
         </nav>
-    </div>
+    </>
+)
 }
 export default NavMenu;
