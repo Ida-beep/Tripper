e2e2a271e4784649fa3908b975543681d7bb79f3
Malene <malenegorm@gmail.com>
2021-12-03 15:27:47 +0100

    added shoppingData

---
+++ src/components/data/shoppingData.js
@@ -0,0 +1,26 @@
+    //state variable, a list of shopping items
+    const shoppingData = [
+
+        {
+            "id": 1,
+            "item": "Mælk",
+            "amount": 6,
+            "unit": "l", 
+        },
+        {
+            "id": 2,
+            "item": "Æg",
+            "amount": 40,
+            "unit": "stk", 
+        },
+
+        {
+            "id": 3,
+            "item": "Mel",
+            "amount": 5, 
+            "unit": "kg",
+        },
+
+    ]
+
+export default shoppingData;
